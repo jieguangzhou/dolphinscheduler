a0aa76acf3f33efb12eb1e45286e55947bf4e59b
jieguangzhou <jieguang_zhou@163.com>
2022-07-12 21:18:42 +0800

    add sagemaker pipeline task plugin

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-sagemaker.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-sagemaker.ts
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { useI18n } from 'vue-i18n'
 import type { IJsonItem } from '../types'
 import { useCustomParams } from '.'
 
